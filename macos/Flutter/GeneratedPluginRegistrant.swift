dccea8cefd56d184ff67174ef1b5dd0683f2a41f
gaorio <gabriele.orio@mahle.com>
2023-03-17 09:13:02 +0100

    work without bluetooth

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_blue_plugin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_blue_plugin/flutter_blue_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterBluePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterBluePlugin"));
 }
